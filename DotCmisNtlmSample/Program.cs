9840b4a434d78ab17dafcf9785a27f399c90501e
Nicolas Raoul <nicolas.raoul@gmail.com>
2013-10-28 17:03:00 +0900

    Implemented NTLM-based SSO sample

--- DotCmisNtlmSample/Program.cs
+++ DotCmisNtlmSample/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DotCMIS;
+using DotCMIS.Client.Impl;
+using DotCMIS.Client;
 
 namespace DotCmisNtlmSample
 {
@@ -9,6 +12,28 @@ namespace DotCmisNtlmSample
     {
         static void Main(string[] args)
         {
+            // Parameters.
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters[SessionParameter.AtomPubUrl] = "http://kate:18080/alfresco/cmisatom"; // Change this to yours.
+            parameters[SessionParameter.BindingType] = BindingType.AtomPub;
+            parameters[SessionParameter.AuthenticationProviderClass] = "DotCMIS.Binding.NtlmAuthenticationProvider";
+            
+            // No need for username and password, thanks to NTLM-based SSO (Single Sign On)
+            //parameters[SessionParameter.User] = "<username>";
+            //parameters[SessionParameter.Password] = "<password>";
+
+            SessionFactory factory = SessionFactory.NewInstance();
+            ISession session = factory.GetRepositories(parameters)[0].CreateSession();
+
+            // List all children of the root folder.
+            IFolder rootFolder = session.GetRootFolder();
+            foreach (ICmisObject cmisObject in rootFolder.GetChildren())
+            {
+                Console.WriteLine(cmisObject.Name);
+            }
+
+            // Don't close console.
+            Console.ReadKey();
         }
     }
 }
